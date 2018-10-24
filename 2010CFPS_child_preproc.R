b0bff5312cbb0f85b471a0b926ffe1e195a6c19a
Caiyuqing <caiyq16@mails.tsinghua.edu.cn>
2018-10-25 01:08:50 +0800

    primary processing

--- 2010CFPS_child_preproc.R
+++ 2010CFPS_child_preproc.R
@@ -54,20 +54,42 @@ child10_15 <- dfc %>%
 ##SES: human
 hCapc <- child10_15 %>%
   dplyr::select(edu2010, wordtest, mathtest, wh9, wd2) # education level in 2010, wordtest, mathtest, education expactancy of child, education expactancy of child's parents
-hCapc$wh9[hCap$wh9 == 9] <- 1 # education expectancy, 9=不必念书, 2-8=小学-博士
+hCapc$wh9[hCapc$wh9 == 9] <- 1 # education expectancy, 9=不必念书, 2-8=小学-博士
 hCapc[hCap < 0 ] <- NA
-summary(hCap)
+summary(hCapc)
 ##SES: material
 mCapc <- child10_15 %>%
-  dplyr::select(moccupisco, foccupiso) # mother occupation, father occupation, family income
-父母的教育期望：d2
-attri: 家长E4,自答m401-m407
-访员观察：z201理解能力，z207智力
-目标：n501-n511
-抑郁：n401-n406，fdepression
-m302幸福
-m303未来信心
-m301，m304人际交往
-？只有一年年龄段的儿童有的数据暂时忽略（如价值观？）
-pol：mparty，fparty
-父母职业：moccupisco，foccupiso
+  dplyr::select(moccupisco, foccupisco) # mother occupation, father occupation, family income
+#INCOME?
+##SES: political
+pCapc <- child10_15 %>%
+  dplyr::select(mparty, fparty) %>%
+  dplyr::mutate(mparty = recode(mparty, "1" = 1, .default = 0),
+         fparty = recode(fparty, "1" = 1, .default = 0)) 
+
+#MH
+MHc <- child10_15 %>%
+# depression, social1, social2, happiness, confidence in future, observation-cognitive, observation-intelligence
+  dplyr::select(depression, wm301, wm304, wm302, wm303, wz201, wz207) 
+MHc[MHc < 0] <- NA
+summary(MHc)
+
+#reliability of depression
+depr <- child10_15 %>%
+  dplyr::select(wn401, wn402, wn403, wn404, wn405, wn406) 
+psych::alpha(depr)
+
+#attribution
+attric <- child10_15 %>%
+  dplyr::select(wm401, wm402, wm403, wm404, wm405, wm406, wm407,  #children
+                we401, we402, we403, we404, we405, we406, we407)  #parents answer for children
+attric[attric < 0] <- NA
+summary(attric) #####NOTICE: many missing values
+
+#Goal
+goalc <- child10_15 %>%
+  dplyr::select(wn501, wn502, wn503, wn504, wn505, wn506, wn507, wn508, wn509, wn510, wn511)
+goalc[goalc < 0] <- NA
+summary(goalc) #####NOTIC: many missing values
+
+##?father's pid and mother's pid--parents SES and child's happiness
