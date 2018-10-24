68e38dd56a9571f088a9b640b94b0d7267fa5337
Caiyuqing <caiyq16@mails.tsinghua.edu.cn>
2018-10-24 17:16:09 +0800

    latest version

--- CFPS2010_adult_preproc.R
+++ CFPS2010_adult_preproc.R
@@ -1,152 +1,153 @@
-################## Preprocessing CFPS data ##############################
+################## Preprocessing CFPS adult data ##############################
 # 
 # Author      Date(yy-mm-dd)   Change History
 #==========================================
 # Cai, Y-Q    18-09-29         The first version
 # Hu, C-P     18-09-30         Add notes, generalizing to other env.
-# 
-#
-#
-#
+# Cai, Y-Q    18-10-09         Only include SES and mental health, ignore physical ones
+# Cai, Y-Q    18-10-21         Add fairness and attributional styles
+# Hu, C-P     18-10-22         Review the code, change the absolute path to relative path, and other comments
+# Cai, Y-Q    18-10-24         Confirm the file and change some dimensions
 ###### input######
 # [CFPS Public Data] 2010 Adult Data (ENG).tab
 # [CFPS Public Data] 2010 Family Data (ENG).tab
 # [CFPS Public Data] 2010 Family Roster Data (ENG).tab
 
 ###### output #####
-#
+#sesMH.csv
 #
 #
 #
 ######################## Start of the script ###########################
-#set directory to the folder of analytic data
+### clean the memory to avoid unnecessary errors:
+rm(list = ls())
 
-# Get the current working directory in Rstudio
+### set directory to the folder of analytic data
+
+# Get the directory of the current R script
 curWD <- dirname(rstudioapi::getSourceEditorContext()$path)
+
+# Set the working directory to the directory where this script is 
 setwd(curWD)
-# setwd("/Users/apple/Desktop/CFPS/4_analysis")
-# getwd()
+
+# load the packages needed, if not exist, download from cran
+if (!require(tidyverse)) {install.packages("tidyverse",repos = "http://cran.us.r-project.org"); require(tidyverse)}
+if (!require(dplyr)) {install.packages("dplyr",repos = "http://cran.us.r-project.org"); require(dplyr)}
+if (!require(psych)) {install.packages("psych",repos = "http://cran.us.r-project.org"); require(psych)}
+library("psych")
+library("dplyr")
+library("tidyverse")
 
 #read data
-dfa<-read.table("[CFPS Public Data] 2010 Adult Data (ENG).tab", sep="\t",header=T)#adult 2010
-dff<-read.table("[CFPS Public Data] 2010 Family Data (ENG).tab", sep="\t",header=T)#family 2010
-dfrf<-read.table("[CFPS Public Data] 2010 Family Roster Data (ENG).tab", sep="\t",header=T)#family roster
-
-#ID information
-summary(dfa$qa1age)#age
-summary(dfa$qa2==1)
-summary(dfa$qa2==3)#hukou
+dfa <- read.table("/Users/apple/Desktop/CFPS/4_Analysis/[CFPS Public Data] 2010 Adult Data (ENG).tab", sep="\t",header=T)#adult 2010
 
 ###### get related variables
-# list of variables and corresponding column names
-idName <- c("pid","fid")
-hCapname <- c("educ","wordtest","mathtest") #human: education and cognitive
-mCapname1 <- c("qg307isei","qg307siops","qg307egp") #material capitl - occupation
-mCapname2=c("fid","ff601","ff701","ff8")#material-income
-memberinfoname <- c("t1_c1","t1_c2","t1_c3","t1_c4","t1_c5","t1_c6","t1_c7","t1_c8","t1_c9","t1_c10","t1_f","t1_m","t1_s") #pid, fid, father, mother, spouse and children
-Health_p_name <- c("qp3","qx3","qx4","qx5","qx6","qz202","qq2","qq3") #self-reated, limitation, observed, smoke and drink
-Health_m_name <- c("qq601","qq602","qq603","qq604","qq605","qq606","qk802","qm404","qm403")
+#ID information
+persinfo <- dfa %>%
+  dplyr::select(gender, qa1age)
 
-# select data
-df.sesMH <- 
-      
-### SES variables
-## Human Capital:  education and cognitive
-#humanCapname <- c("pid","fid","educ","wordtest","mathtest") #human: education and cognitive
-humanCap <- dfa[, c("pid","fid","educ","wordtest","mathtest")]#extract data
-humanCap$wordtest[humanCap$wordtest==-8]<-NA
-humanCap$mathtest[humanCap$mathtest==-8]<-NA #replace none existed data
-summary(humanCap) #check human
+#summarize personal informations
+summary(dfa$qa1age)  #age
+summary(dfa$gender)  #gender
+summary(dfa$qa2==1)  #hukou: agriculture
+summary(dfa$qa2==3)  #hukou: non-agriculture
 
-## Material captial
-# Material captial 1: occupation
-mCapname1=c("pid","fid","qg307isei","qg307siops","qg307egp") #material capitl - occupation
-mCap1 <- dfa[,mCapname1] #extractdata
-mCap1$qg307egp[mCap1$qg307egp<0] <-NA
-mCap1$qg307egp[mCap1$qg307egp>11] <-NA #replace none existed data
-summary(mCap1) #check material1
+# Select and recode SES
+SES <- dfa %>%
+  dplyr::select(fid, pid, educ, wordtest, mathtest, qg307isco, qg307isei, qk601,qa7_s_1) %>%
+  dplyr::mutate(qa7_s_1 = recode_factor(qa7_s_1, '1' = 1, .default = 0)) #leave only communist members
+SES[SES == -8] <- NA #missing values
+summary(SES) # check SES
 
-# Material captial 2: income
-mCapname2=c("fid","ff601","ff701","ff8")#material-income
-mCap2 <- dff[,mCapname2] #extract data
-mCap2$ff601[mCap2$ff601<0]<-NA
-mCap2$ff701[mCap2$ff701<0]<-NA
-mCap2$ff8[mCap2$ff8<0]<-NA #replace none existed data
-summary(mCap2) #check material2
+# set qa7_s_1 = 1 if any family member is a communist party member, else = 0
+pCap <- SES %>%
+  dplyr::select(qa7_s_1, fid) %>%
+  dplyr::mutate(qa7_s_1 = recode_factor(qa7_s_1, '1' = 1, .default = 0)) %>%
+  dplyr::filter(qa7_s_1 == 1)
+SES <- SES %>%
+  dplyr::select(-qa7_s_1) %>%
+  dplyr::left_join(pCap, SES, by = "fid")
+SES$qa7_s_1[is.na(SES$qa7_s_1)] <- 0
 
-# calculate the income per capita/member
-memberinfoname <- c("t1_c1","t1_c2","t1_c3","t1_c4","t1_c5","t1_c6","t1_c7","t1_c8","t1_c9","t1_c10","t1_f","t1_m","t1_s","pid","fid") #pid, fid, father, mother, spouse and children
-memberinfo <- dfrf[,memberinfoname] #extract data
-membername <- c("t1_c1","t1_c2","t1_c3","t1_c4","t1_c5","t1_c6","t1_c7","t1_c8","t1_c9","t1_c10","t1_f","t1_m","t1_s") # father, mother, spouse and children
-member <- dfrf[,membername]
-member[member==-8]<-NA #replace none existed data
-summary(member) #check member
-memberinfo$number <- rowSums(member,na.rm=TRUE,dims=1)+1 #count family member, include the participant
+#delate repatitive rows
+SES <- SES %>%
+  dplyr::group_by(pid) %>%
+  dplyr::filter(row_number() == 1) %>%
+  dplyr::ungroup()
 
-#merge member with material2
-data <- merge(mCap2, memberinfo, by="fid")
+# recode income using log10, if income =0, recode it as 0 
+SES <- SES %>%
+  dplyr::mutate(qk601 = log10(qk601)) %>%
+  dplyr::mutate(qk601 = recode_factor(qk601, '-Inf' = 0 ))
 
-#calculate income per capita
-incomename <- c("ff601","ff701","ff8") #income variables
-income <- data[,incomename]#extract data
-data$allincome <- rowSums(income,na.rm=T,dims=1)#sum income
-data$perincome <- data$allincome/data$number #per capita income
-summary(data$perincome) #check perincome
-# tempname<-  #perincome variables
-temp <- data[,c("fid","pid","perincome")]#extract data
-mCap <-merge(mCap1,temp, by="pid") #merge occupation and income
-mCap$fid=mCap$fid.x #replace fid.x fid.y with fid
-mCap$fid.x<-NULL
-mCap$fid.y<-NULL #delete fid.x and fid.y
-summary(mCap)#check material
+# select and recode mental health, fairness and attributional style
+##MH
+MH <- dfa %>%
+  dplyr::select(qm403, qm404, qk802, depression)
+MH[MH == -8] <- NA
+MH$qm403[MH$qm403 <0] <- NA
+MH$qm404[MH$qm404 <0] <- NA
+summary(MH)
 
-## political captial
-#pCap=c("pid","fid","qa7_s_1")#political
-pCap <- dfa[,c("pid","fid","qa7_s_1")] #extract data
-pCap$qa7_s_1[pCap$qa7_s_1!=1]<-0 #leave only communist party
-summary(pCap) #check politic
+# reliability of depression scale
+depr <- dfa %>%
+  dplyr::select(qq601, qq602, qq603, qq604, qq605, qq606)
+depr[depr == -8 ] <- NA
+depr[depr == -2 ] <- NA
+depr[depr == -1] <- NA
+summary(depr)
+psych::alpha(depr)
+depr <- depr %>%
+  dplyr::filter(complete.cases(depr))
+#???
+stats::factanal(depr, factors=2) 
+scree(depr)
 
-# add politic by family
-x<-aggregate(qa7_s_1~fid,data=pCap,sum)
-#merge x with politic
-pol_f <- merge(x, pCap, by="fid")
-#extract politic variables
-#politicfamilyname=c("fid","pid","qa7_s_1.x")
-politicfamily=pol_f[,c("fid","pid","qa7_s_1.x")]#extract data
-summary(politicfamily) #check politicfamily
+##fairness
+fair <- dfa %>%
+  dplyr::select(qn201, qn202, qn203, qn204, qn205, qn206, qn207, qn208) 
+fair[fair < 0] <- NA
+fair[fair > 5] <- NA
+fair[fair == 5] <- 0 # encounter with unfair affairs? 1 = yes, 5 = no
+scree(fair)
+summary(fair)
+library(psych)
+fair  %>%
+  dplyr::filter(complete.cases(fair)) %>%
+  # factor analysis?
+  stats::factanal(fair, factors=4)
+# reliability of fairness scale
+psych::alpha(fair)
 
-#merge SES
-SES <- merge(humanCap,mCap,by="pid")
-SES <- merge(SES,politicfamily, by="pid")
-#clear useless variables
-SES$fid.x<-NULL
-SES$fid.y<-NULL
+# sum of fairness, add to MH
+MH$fairsum <- rowSums(fair)
 
-###################################################
-###################################################
-##health variables
-#physical
-Health_p_name <- c("qp3","qx3","qx4","qx5","qx6","qz202","qq2","qq3","pid","fid") #self-reated, limitation, observed, smoke and drink
-phy=dfa[,physicalname] #extract data
-phy[phy<0]<-NA #clear non-existed data
-summary(phy) #check physical
-#mental
-mentalname=c("qq601","qq602","qq603","qq604","qq605","qq606","qk802","qm404","qm403","pid","fid")
-mental=dfa[,mentalname]#extract data
-mental[mental<0]<-NA #clear non-existed data
-summary(mental)#check mental
-#merge health
-health<- merge(mental,phy, by=c("pid","fid"))
-#merge SES and health
-sesandhealth<-merge(SES, health, by=c("pid","fid"))
-#export table
-write.csv(sesandhealth, file="SESandHEALTH.csv",row.names=FALSE)
-#reliability of depression scale
-#install.packages("userfriendlyscience")##if necessary, delete "#"
-library("userfriendlyscience")
-deprname=c("qq601","qq602","qq603","qq604","qq605","qq606")
-depr=dfa[,deprname]
-reliability(depr,
-            items = NULL,
-            itemDiagnostics = FALSE,
-            digits = 2)
+##attribute
+attri <- dfa %>% 
+  dplyr::select(qn501, qn502, qn503, qn504, qn505, qn506, qn507)  # 1-4, strongly disagree - strongly agree
+summary(attri)
+
+### recode attribute 1-4= strongly diagree, disagree, agree, strongly agree, 5=not agree not disagree, 6=i don't know, -8=missing value
+attri <- attri %>%
+  dplyr::mutate(qn501 = recode(qn501, '1' = -2, '2' = -1 ,'5' = 0, '3' = 1, '4' = 2, .default = -8),
+                qn502 = recode(qn502, '1' = -2, '2' = -1 ,'5' = 0, '3' = 1, '4' = 2, .default = -8),
+                qn503 = recode(qn503, '1' = -2, '2' = -1 ,'5' = 0, '3' = 1, '4' = 2, .default = -8),
+                qn504 = recode(qn504, '1' = -2, '2' = -1 ,'5' = 0, '3' = 1, '4' = 2, .default = -8),
+                qn505 = recode(qn505, '1' = -2, '2' = -1 ,'5' = 0, '3' = 1, '4' = 2, .default = -8),
+                qn506 = recode(qn506, '1' = -2, '2' = -1 ,'5' = 0, '3' = 1, '4' = 2, .default = -8),
+                qn507 = recode(qn507, '1' = -2, '2' = -1 ,'5' = 0, '3' = 1, '4' = 2, .default = -8))
+attri  %>%
+  dplyr::filter(complete.cases(attri)) %>%
+  # factor analysis?
+  stats::factanal(attri, factors=2) 
+# reliability of fairness scale
+psych::alpha(attri)
+
+
+#reliability of attribute scale
+psych::alpha(attri)
+
+# combine all the related data
+all <- base::cbind(persinfo, SES, MH, attri)
+# write the table
+write.csv(all, file = "sesMH.csv", row.names = FALSE)
