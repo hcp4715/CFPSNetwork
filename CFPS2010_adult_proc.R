06b9e708b845b4cc65dc732c9b1a7d9a730a183d
Caiyuqing <caiyq16@mails.tsinghua.edu.cn>
2018-10-25 08:13:46 +0800

    latest version

--- CFPS2010_adult_proc.R
+++ CFPS2010_adult_proc.R
@@ -4,8 +4,8 @@
 #==========================================
 # Cai, Y-Q    18-10-09         First version
 # HCP         18-10-11         Add code to remove the rows with NA and suggestion for coding
-# Cai, Y-Q    18-10-22         Divide by gender
-#
+# Cai, Y-Q    18-10-20         Divide by gender
+# Cai, Y-Q    18-10-24         Change dimension of depr and fair
 ###### input######
 #sesMH.csv
 #
@@ -34,41 +34,39 @@ setwd(curWD)
 
 if (!require(qgraph)) {install.packages("qgraph",repos = "http://cran.us.r-project.org"); require(qgraph)}
 library("qgraph")
-
 if (!require(tidyverse)) {install.packages("tidyverse",repos = "http://cran.us.r-project.org"); require(tidyverse)}
-library("tidyverse")  ## hcp: use tidyverse style for the code
-
-library("parcor")
+library("tidyverse")  
+if (!require(parcor)) {install.packages("parcor",repos = "http://cran.us.r-project.org"); require(parcor)}
+library("parcor")  
+if (!require(Matrix)) {install.packages("Matrix",repos = "http://cran.us.r-project.org"); require(Matrix)}
 library("Matrix")
+if (!require(psych)) {install.packages("psych",repos = "http://cran.us.r-project.org"); require(psych)}
 library("psych")
+if (!require(dplyr)) {install.packages("dplyr",repos = "http://cran.us.r-project.org"); require(dplyr)}
 library("dplyr")
-library("ggplot2")
+
 
 #set directory to the folder of analytic data
 data<-read.csv("sesMH.csv",header=T)     
-#setwd("/Users/apple/Desktop/CFPS/5_ProcessData") 
-#getwd()
+setwd("/Users/apple/Desktop/CFPS/5_ProcessData") 
+getwd()
 
 summary(data)
 ##NOTICE: over 10000 missing values for occupation coding and 1865 for income, thousands missing for attributional style and fairness
 data.complete <- data %>%     
-      dplyr::filter(complete.cases(data)) %>%  # only the complete cases (i.e., no NA in every row) 
-      dplyr::select(-fid, -pid, -gender, -qa1age) # drop the columns for later analysis
+  dplyr::filter(complete.cases(data)) %>%  # only the complete cases (i.e., no NA in every row) 
+  dplyr::select(-fid, -pid, -gender, -qa1age) # drop the columns for later analysis
 
 # label for the nodes of network
-labels <- c("attri1", "attri2", "attri3", "attri4", "attri5", "attri6", "attri7", 
-            "fair1", "fair2", "fair3", "fair4", "fair5", "fair6", "fair7", "fair8", 
-            "educ", "word", "math", "occu1", "occu2", "income", "pol",
-            "depr1", "depr2", "depr3", "depr4", "depr5", "depr6", "happ", "satis", "confi")
-
+labels <- c("educ", "word", "math", "occu1", "occu2", "income", "pol",
+            "satis","confi", "happi", "depr", "fair", "attri1", "attri2", "attri3", "attri4", "attri5", "attri6", "attri7")
 ##NOTICE: data.complete has 9419 obs
 #generate partial correlation network of all the data
 
-groups <- factor(c(rep("attri", 7), rep("fair", 8), 
-                   rep("hCap", 3), rep("mCap", 3), rep("pCap", 1), 
-                   rep("deprMH", 6), rep("otherMH", 3)))
+groups <- factor(c(rep("hCap", 3), rep("mCap", 3), rep("pCap", 1), 
+                   rep("MH", 4), rep("otherMH", 8)))
 netPcor <- qgraph::qgraph(cor(data.complete), layout = "spring", 
-                    labels = labels, groups = groups, graph = "concentration")
+                          labels = labels, groups = groups, graph = "concentration")
 # export image
 jpeg(file = "/Users/apple/Desktop/CFPS/5_ProcessData/all.jpeg")
 plot(netPcor)
@@ -76,26 +74,25 @@ dev.off()
 
 # data without those variables that have too many missing values
 data.more <- data %>%
-      dplyr::select(qq601, qq602, qq603, qq604, qq605, qq606, qm403, qm404, qk802, educ, wordtest, mathtest, qa7_s_1) %>%
-      dplyr::filter(complete.cases(data))
+  dplyr::select(fdepression, qm403, qm404, qk802, educ, wordtest, mathtest, qa7_s_1) %>%
+  dplyr::filter(complete.cases(data))
 summary(data.more)
 
 # generate partial correlation network of male, 30-50
 data.male <- data %>%
-      dplyr::filter(complete.cases(data)) %>% 
-      dplyr::filter(gender == 1) %>% # male
-      dplyr::filter(qa1age >= 30 & qa1age <=50) %>% # age:30-50
-      dplyr::group_by(fid) %>% 
-      dplyr::filter(pid == max(pid)) %>%  # one each family
-      dplyr::select(-pid, -gender, -qa1age)  # drop the columns for later analysis
+  dplyr::filter(complete.cases(data)) %>% 
+  dplyr::filter(gender == 1) %>% # male
+  dplyr::filter(qa1age >= 30 & qa1age <=50) %>% # age:30-50
+  dplyr::group_by(fid) %>% 
+  dplyr::filter(pid == max(pid)) %>%  # one each family
+  dplyr::select(-pid, -gender, -qa1age)  # drop the columns for later analysis
 data.male$fid <- NULL# drop the columns for later analysis 
 summary(data.male)
 
-groups <- factor(c(rep("attri", 7), rep("fair", 8), 
-                   rep("hCap", 3), rep("mCap", 3), rep("pCap", 1), 
-                   rep("deprMH", 6), rep("otherMH", 3)))
+groups <- factor(c(rep("hCap", 3), rep("mCap", 3), rep("pCap", 1), 
+                   rep("MH", 4), rep("otherMH", 8)))
 netPcor_m <- qgraph(cor(data.male), layout = "spring", 
-                  labels = labels, groups = groups, graph = "concentration")
+                    labels = labels, groups = groups, graph = "concentration")
 
 # export image
 jpeg(file = "/Users/apple/Desktop/CFPS/5_ProcessData/Male.jpeg")
@@ -132,17 +129,16 @@ dev.off()
 
 # generate partial correlation network of female, 30-50
 data.female <- data %>%
-  filter(complete.cases(data)) %>%
-  filter(gender == 0) %>% # male
-  filter(qa1age >= 30 & qa1age <= 50) %>% # age:30-50
-  group_by(fid) %>% 
-  filter(pid == max(pid)) %>% # one each family
-  select(-pid, -gender, -qa1age) # drop the columns for later analysis
+  dplyr::filter(complete.cases(data)) %>%
+  dplyr:: filter(gender == 0) %>% # male
+  dplyr::filter(qa1age >= 30 & qa1age <= 50) %>% # age:30-50
+  dplyr::group_by(fid) %>% 
+  dplyr::filter(pid == max(pid)) %>% # one each family
+  dplyr::select(-pid, -gender, -qa1age) # drop the columns for later analysis
 data.female$fid <- NULL
 
-groups <- factor(c(rep("attri", 7), rep("fair", 8), 
-                   rep("hCap", 3), rep("mCap", 3), rep("pCap", 1), 
-                   rep("deprMH", 6), rep("otherMH", 3)))
+groups <- factor(c(rep("hCap", 3), rep("mCap", 3), rep("pCap", 1), 
+                   rep("MH", 4), rep("otherMH", 8)))
 netPcor <- qgraph(cor(data.female), layout = "spring", labels = labels,
                   groups = groups, graph = "concentration")
 
